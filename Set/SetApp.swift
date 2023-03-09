6ec9f83393e582d15773321745872c7fb6f5e070
JohnA28 <John.Alban41@gmail.com>
2023-03-09 17:20:20 -0500

    Initial Commit

---
+++ Set/SetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetApp.swift
+//  Set
+//
+//  Created by John Alban on 3/9/23.
+//
+
+import SwiftUI
+
+@main
+struct SetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
